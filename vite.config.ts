9bf341b9be407aeb5c216989b9e2a0d77cb0854a
2381031 <2381031@unai.edu>
2026-07-22 21:51:09 +0700

    fix: remove .vercelignore, inline all config into vite.config.ts, use routes handle filesystem

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss({content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}']}),
+        autoprefixer(),
+      ],
+    },
+  },
 });
