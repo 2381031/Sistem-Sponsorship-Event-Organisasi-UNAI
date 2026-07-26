e25da716768f169ce61442fabb1fa4f6d27d6083
2381031 <2381031@unai.edu>
2026-07-26 23:27:12 +0700

    UPDATE5

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import {defineConfig} from 'vite';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+    },
+  },
   css: {
     postcss: {
       plugins: [
